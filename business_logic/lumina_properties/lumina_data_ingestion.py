06de23a1bf0afff24b7b40f51492ee0780e617ef
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-03-04 19:37:22 +0100

    lumina in-progress

--- business_logic/lumina_properties/lumina_data_ingestion.py
+++ business_logic/lumina_properties/lumina_data_ingestion.py
@@ -1,12 +1,12 @@
 import pandas as pd
 from plugins.utils.database import get_db_connection
+from airflow.models import Variable
 
 
 def extract_lumina_data():
+    credentials = Variable.get("postgres_conn_string")
     test_df = pd.read_sql(
         "select * from ay_test.airflow",
-        con=get_db_connection(
-            "postgresql://postgres.lksygmgwphnbbvdbgzaw:pd0S4b4DwgE0IH6L63lq@aws-1-eu-west-1.pooler.supabase.com/postgres"
-        )
+        con=get_db_connection(credentials)
     )
     return test_df
