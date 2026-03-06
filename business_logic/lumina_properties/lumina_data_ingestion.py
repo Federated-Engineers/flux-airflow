c309146b4fef7ebf8c9cbbb6905280011074341d
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-03-06 13:05:13 +0100

    In-progress

--- business_logic/lumina_properties/lumina_data_ingestion.py
+++ business_logic/lumina_properties/lumina_data_ingestion.py
@@ -1,23 +1,3 @@
-# import pandas as pd
-# from airflow.models import Variable
-# from plugins.utils.database import get_db_connection
-
-
-# def extract_lumina_data():
-#     credentials = Variable.get("postgres_conn_string")
-#     test_df = pd.read_sql(
-#         "select * from ay_test.airflow",
-#         con=get_db_connection(credentials)
-#     )
-#     return test_df
-
-
-
-
-
-
-
-from fileinput import filename
 import os
 
 import pandas as pd
@@ -30,12 +10,12 @@ def extract_lumina_data():
     conn=get_db_connection(credentials)
     
     temp_dir = "/opt/airflow/temp"
-    os.makedir(temp_dir, exist_ok=True)
+    os.makedirs(temp_dir, exist_ok=True)
 
     tables= [
         "ay_test.airflow",
         "ay_test.property",
-        "renovation_legders"
+        "ay_test.renovation_legders"
     ]
 
     for table_name in tables:
@@ -45,17 +25,24 @@ def extract_lumina_data():
             chunk_filter = pd.read_sql(query, con=conn, chunksize=40000)
             
             for i, chunk_df in enumerate(chunk_filter):
-                local_file = f"opt/airflow/temp/{table_name}_batch_{i}.parquet"
+                local_file = os.path.join(temp_dir, f"{table_name}_batch_{i}.parquet")
+
                 
                 # to local
                 chunk_df.to_parquet(local_file)
 
+
                 # os.remove(local_file)
 
         except Exception as e:
-            raise ValueError(f"Could not read {table_name}. It might have been dropped or renamed. contact 'Rofiat'{e}")
+            raise ValueError(
+                f"Could not read {table_name}. Error: {str(e)}"
+                )
 
 
 
     return "Extraction was Successful"
 
+
+# import os
+# print(dir(os))
