a17f370afb681f7f80210ab8ed2e2f1fb15f70e8
Ayodeji Agunbiade <aayo.agunbiade@gmail.com>
2026-03-10 19:42:07 +0100

    70% ready to go

--- business_logic/lumina_properties/lumina_data_ingestion.py
+++ business_logic/lumina_properties/lumina_data_ingestion.py
@@ -1,6 +1,9 @@
 import os
 
 import pandas as pd
+import awswrangler as wr
+# from airflow.providers.amazon.aws.hooks.s3 import S3HooK
+from datetime import datetime
 from airflow.models import Variable
 from plugins.utils.database import get_db_connection
 
@@ -8,30 +11,36 @@ from plugins.utils.database import get_db_connection
 def extract_lumina_data():
     credentials = Variable.get("postgres_conn_string")
     conn = get_db_connection(credentials)
+    bucket_name = "demo-03-bucket"
 
-    temp_dir = "/opt/airflow/temp"
-    os.makedirs(temp_dir, exist_ok=True)
+    today = datetime.now().strftime('%Y-%m-%d')
 
     tables = [
-        "historical.historical_transactions",
-        "historical.property_metadata",
-        "historical.renovation_ledgers",
+        # "historical.historical_transactions",
+        # "historical.property_metadata",
+        # "historical.renovation_ledgers",
         "historical.neighborhood_demographics",
         "historical.zoning_permits"
     ]
 
     for table_name in tables:
+        s3_folder = table_name.replace('.', '_')
+        
         query = f"SELECT * FROM {table_name}"
 
         try:
-            chunk_filter = pd.read_sql(query, con=conn, chunksize=40000)
+            chunk_filter = pd.read_sql(query, con=conn, chunksize=50)
 
             for i, chunk_df in enumerate(chunk_filter):
-                local_file = os.path.join(temp_dir, f"{table_name}_batch_{i}.parquet")
+               
+                s3_path = f"s3://{bucket_name}/raw_data/{s3_folder}/{table_name}_{today}_{i}.parquet"
 
-                chunk_df.to_parquet(local_file)
-
-                os.remove(local_file)
+                wr.s3.to_parquet(
+                    df=chunk_df,
+                    path=s3_path,
+                    index=False
+                )
+                print(f"Uploaded to:{s3_path}")
 
         except Exception as e:
             raise ValueError(
