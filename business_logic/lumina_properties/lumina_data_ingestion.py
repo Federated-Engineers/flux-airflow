9ef6dddf026204057e05a66d7a4b545aea4dc5c5
Akinmeji_Olalekan <akinmejiolalekan7@gmail.com>
2026-03-11 23:56:09 +0100

    Completed the dags script and modified lumina data ingestion script

--- business_logic/lumina_properties/lumina_data_ingestion.py
+++ business_logic/lumina_properties/lumina_data_ingestion.py
@@ -1,16 +1,32 @@
 import os
-
 import pandas as pd
+import boto3
+import logging
+
+from dotenv import load_dotenv
+from io import BytesIO
 from airflow.models import Variable
 from plugins.utils.database import get_db_connection
 
+load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 def extract_lumina_data():
-    credentials = Variable.get("postgres_conn_string")
-    conn = get_db_connection(credentials)
+    logger.info("Starting Lumnina data extration process")
 
-    temp_dir = "/opt/airflow/temp"
-    os.makedirs(temp_dir, exist_ok=True)
+    credentials = Variable.get("postgres_conn_string")
+    # credentials = os.getenv("postgres_conn_string")
+    engine = get_db_connection(credentials)
+
+    s3_bucket = "demo-03-bucket"
+    s3_prefix = "historical"
+    s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_DEFAULT_REGION")
+)
 
     tables = [
         "historical_transactions",
@@ -20,23 +36,69 @@ def extract_lumina_data():
         "zoning_permits"
     ]
 
+    partition_columns = {
+        "historical_transactions": "sale_date",
+        "zoning_permits": "application_date"
+    }
+
     for table_name in tables:
-        query = f"SELECT * FROM {table_name}"
+
+        logger.info(f"Starting extraction for table: {table_name}")
+
+        query = f"SELECT * FROM historical.{table_name}"
 
         try:
-            chunk_filter = pd.read_sql(query, con=conn, chunksize=40000)
+            with engine.connect() as conn:
+                raw_conn = conn.connection
+                chunk_filter = pd.read_sql_query(query, con=raw_conn, chunksize=40000)
 
-            for i, chunk_df in enumerate(chunk_filter):
-                local_file = os.path.join(temp_dir, f"{table_name}_batch_{i}.parquet")
+                for i, chunk_df in enumerate(chunk_filter):
 
-                # to local
-                chunk_df.to_parquet(local_file)
+                    if table_name in partition_columns:
+                        date_column = partition_columns[table_name]
 
-                # os.remove(local_file)
+                        logger.info(f"Applying partitioning using column: {date_column}")
+
+                        chunk_df[date_column] = pd.to_datetime(chunk_df[date_column], errors="coerce")
+
+                        chunk_df["year"] = chunk_df[date_column].dt.year
+                        chunk_df["month"] = chunk_df[date_column].dt.month
+
+                        for (year, month), partition_df in chunk_df.groupby(["year", "month"]):
+
+                            buffer = BytesIO()
+
+                            partition_df.to_parquet(buffer, index=False)
+
+                            buffer.seek(0)
+
+                            s3_key = f"{s3_prefix}/{table_name}/year={year}/month={month}/{table_name}_{i}.parquet"
+
+                            s3.upload_fileobj(buffer, s3_bucket, s3_key)
+
+                            logger.info(f"Uploaded file to s3://{s3_bucket}/{s3_key}")
+
+                    else:
+                        logger.info(f"Uploading non-partitioned chunk {i} for table {table_name}")
+
+                        buffer = BytesIO()
+
+                        chunk_df.to_parquet(buffer, index=False)
+
+                        buffer.seek(0)
+
+                        s3_key = f"{s3_prefix}/{table_name}/{table_name}_batch{i}.parquet"
+
+                        s3.upload_fileobj(buffer, s3_bucket, s3_key)
+
+                        logger.info(f"Uploaded file to s3://{s3_bucket}/{s3_key}")
+
+                    logger.info(f"Finished processing table: {table_name}")
 
         except Exception as e:
-            raise ValueError(
-                f"Could not read {table_name}. Error: {str(e)}"
-                )
+                logger.error(f"Error occurred while processing table {table_name}: {str(e)}")
+                raise ValueError(f"Could not read {table_name}. Error: {str(e)}")
+        
+    logger.info("Lumina data extraction completed successfully")
 
     return "Extraction was Successful"
--- dags/lumina_brick/extract_data.py
+++ dags/lumina_brick/extract_data.py
@@ -1,20 +1,31 @@
-import datetime
+import sys
+import os
 
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.sdk import DAG
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
+from datetime import datetime, timedelta
+from airflow.decorators import dag, task
+
 from business_logic.lumina_properties.lumina_data_ingestion import \
     extract_lumina_data
 
-my_dag = DAG(
-    dag_id="lumina",
-    start_date=datetime.datetime(2026, 3, 4),
-    schedule="@daily",
-)
 
-lumina_pipeline = PythonOperator(
-    dag=my_dag,
-    python_callable=extract_lumina_data,
-    task_id="lumina_pipeline"
-    )
+default_args = {
+    "owner": "flux",
+    "depends_on_past": False,
+    "email_on_failure": True,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+    "execution_timeout": timedelta(hours=2),
+}
 
-lumina_pipeline
+@dag(dag_id="flux_dag",default_args=default_args, start_date=datetime(2026, 3, 8), schedule= "@daily", catchup=False)
+def lumina_pipeline():
+    @task()
+    def run_extraction():
+        lumina_data = extract_lumina_data()
+        return lumina_data
+    
+    run_extraction()
+
+lumina_pipeline()
