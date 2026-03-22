5c641cc12a4cdffc4bdca0f23f2cdc11c5f23906
Akinmeji_Olalekan <akinmejiolalekan7@gmail.com>
2026-03-22 21:30:19 +0100

    obey isort rule

--- business_logic/lumina_properties/lumina_data_ingestion.py
+++ business_logic/lumina_properties/lumina_data_ingestion.py
@@ -2,8 +2,8 @@ import logging
 
 import awswrangler as wr
 import pandas as pd
-
 from airflow.models import Variable
+
 from plugins.utils.database import get_db_connection
 
 logger = logging.getLogger(__name__)
