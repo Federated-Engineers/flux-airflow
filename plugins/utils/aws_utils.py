0e4e14d209273f5cf450c7e7d184aa33569eb07f
engineer-vicola <akinbami.victor@gmail.com>
2026-04-09 11:01:54 +0100

    updated script

--- plugins/utils/aws_utils.py
+++ plugins/utils/aws_utils.py
@@ -1,14 +1,6 @@
-import os
 import boto3
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def new_session():
-    boto3.session.Session(
-            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-            region_name='eu-central-1'
-    )
 
-
+    return boto3.Session()
