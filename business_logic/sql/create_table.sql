527d9b95018205e2f04929240e4f81dc73049297
engineer-vicola <akinbami.victor@gmail.com>
2026-04-09 11:01:54 +0100

    updated script

---
+++ business_logic/sql/create_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS nordic_retail (
+    order_id VARCHAR(50),
+    warehouse_origin VARCHAR(50),
+    warehouse_zone VARCHAR(50),
+    carrier VARCHAR(50),
+    priority VARCHAR(50),
+    is_disputed BOOLEAN,
+    api_version VARCHAR(50),
+    ingested_at TIMESTAMP,
+    ingested_date DATE,
+    ingested_year SMALLINT,
+    ingested_month SMALLINT,
+    ingested_day SMALLINT
+);
