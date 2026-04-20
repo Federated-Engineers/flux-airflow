6ae0f72f126bbaebe24b6b4b85b94d45cf4df3b9
RofiatAbdulkareem <oyizakareem9@gmail.com>
2026-04-20 09:36:18 +0100

    added the dag

---
+++ dags/nordic_retail_collective/sql/create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS logistics (
+    order_id VARCHAR(50),
+    warehouse_origin VARCHAR(50),
+    warehouse_zone VARCHAR(50),
+    carrier VARCHAR(50),
+    priority VARCHAR(50),
+    is_disputed BOOLEAN,
+    api_version VARCHAR(50),
+    ingested_at TIMESTAMP,
+    ingested_date DATE
+);
