083f4a0e3faa6d2c597c359647a8bb2223fbaebd
Chichi126 <chinyere.nwigwe126@gmail.com>
2026-04-27 00:15:40 +0100

    edited datetime scripts

--- plugins/date_parts.py
+++ plugins/date_parts.py
@@ -4,7 +4,9 @@ import datetime
 def get_current_date_parts():
     """Get current date for partitioning in s3"""
     now = datetime.datetime.now()
-    year = now.year
-    month = now.month
-    day = now.day
-    return year, month, day
+    return {
+        'year': now.year,
+        'month': now.month,
+        'day': now.day
+    }
+
