02e3c440c22de9bc4a703a123e730690524ac606
Chichi126 <chinyere.nwigwe126@gmail.com>
2026-04-26 23:32:16 +0100

    edit

--- business_logic/riviera_soleil/riviera_data_ingestion.py
+++ business_logic/riviera_soleil/riviera_data_ingestion.py
@@ -14,17 +14,17 @@ def ingest_riviera_data_into_s3():
     BUCKET_NAME = riviera_config["BUCKET_NAME"]
     DATABASE_NAME = riviera_config["DATABASE_NAME"]
 
-    dt = get_current_date_parts()
-    df['year'] = dt['year']
-    df['month'] = dt['month']
-    df['day'] = dt['day']
-
     get_ssm_parameter(FILE_PATH)
     all_sheets_data = connect_get_data_from_google_sheet(SHEET_NAME, FILE_PATH)
     for sheet_data in all_sheets_data:
         df = sheet_data["df"]
+        dt = get_current_date_parts()
+        df['year'] = dt['year']
+        df['month'] = dt['month']
+        df['day'] = dt['day']
         TABLE_NAME = sheet_data["FILE_NAME"]
         load_data_to_s3(
-            BUCKET_NAME, DATABASE_NAME, TABLE_NAME, dt['year'], dt['month'], dt['day'], df
+            BUCKET_NAME, DATABASE_NAME, TABLE_NAME,
+            dt['year'], dt['month'], dt['day'], df
         )
     return "Data loaded successfully"
