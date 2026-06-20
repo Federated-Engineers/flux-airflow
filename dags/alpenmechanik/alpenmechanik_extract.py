8584a9ef8e6d34d7291c33febaca03ed4acbd2e9
Akinmeji_Olalekan <akinmejiolalekan7@gmail.com>
2026-06-20 11:36:22 +0100

    lambda script added and the dag script rewritten for lambda

--- dags/alpenmechanik/alpenmechanik_extract.py
+++ dags/alpenmechanik/alpenmechanik_extract.py
@@ -1,28 +1,29 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
-from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG
-from business_logic.alpenmechanik.alpenmechanik_data import run_pipeline
+from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
 
-DAG_ID = 'alpenmechanik_data_AG'
+DAG_ID = "alpenmechanik_data_AG"
 
 default_args = {
     "owner": "alpenmechanik_data_AG",
     "retries": 2,
     "retry_delay": timedelta(minutes=2),
+    "execution_timeout": timedelta(minutes=10),
 }
 
-dag = DAG(
-    dag_id="alpenmechanik_data_AG",
+with DAG(
+    dag_id=DAG_ID,
     default_args=default_args,
     schedule="0 0 * * *",
+    start_date=datetime(2026, 6, 13),
     catchup=False,
-)
+    tags=["alpenmechanik"],
+) as dag:
 
-write_data_to_s3 = PythonOperator(
-    dag=dag,
-    python_callable=run_pipeline,
-    task_id="write_data_to_s3"
-    )
-
-write_data_to_s3
+    trigger_lambda = LambdaInvokeFunctionOperator(
+        task_id="trigger_sheet_transformer",
+        function_name="sheet-transformer",
+        aws_conn_id="aws_default",
+        invocation_type="RequestResponse",
+        log_type="Tail",) 
